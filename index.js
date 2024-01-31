b0779a63f1e02a8e3a3b31b3cfd59cb0f403a306
martin <martin@comets.kr>
2024-01-31 21:04:22 +0900

    fix: modify root file

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const cluster = require("cluster");
-const instanceCPUs = require("os").cpus();
-
-if (cluster.isMaster) {
-  console.log(`MASTER thread with process id => ${process.pid}`);
-
-  instanceCPUs.map((ele) => {
-    cluster.fork();
-  });
-} else {
-  console.log(`WORKER thread with process id => ${process.pid}`);
-}
+console.log("MIT SILA");
